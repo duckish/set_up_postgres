fa9ad6fc92d8bc7f94aef50d04d6db61421734c0
duckish <nguyen_vanduc@hotmail.com>
2020-04-22 17:25:43 -0400

    Create set_up_python.sh

---
+++ set_up_python.sh
@@ -0,0 +1,8 @@
+#!/bin/sh -e
+
+apt-get install libpq-dev
+
+apt-get install python3 python-dev python3-dev \
+     build-essential libssl-dev libffi-dev \
+     libxml2-dev libxslt1-dev zlib1g-dev \
+     python3-pip
