0a98d7652bd43b6b428d101bfee95e81a3591ab8
duckish <nguyen_vanduc@hotmail.com>
2020-04-23 17:25:14 -0400

    Update set_up_python.sh

--- set_up_python.sh
+++ set_up_python.sh
@@ -2,7 +2,7 @@
 
 apt-get install libpq-dev
 
-apt-get install python3 python3-dev \
+apt-get install -y python3 python3-dev \
      build-essential libssl-dev libffi-dev \
      libxml2-dev libxslt1-dev zlib1g-dev \
      python3-pip
